d3418e717d9ca5bd0e0d037db38c57c7008f28d6
maninder-singh <nsit.maninder@gmail.com>
2015-07-28 11:42:22 +0530

    Add PERSIST_FILE_NAME,SAVE_ON_LOCAL_STORAGE,SAVE_ON_DATABASE,SEND_OVER_SERVICE to Constant file,

--- src/main/java/com/imaginea/crawler/constant/Constant.java
+++ src/main/java/com/imaginea/crawler/constant/Constant.java
@@ -5,4 +5,9 @@ public class Constant {
 	public static final String URL = "http://mail-archives.apache.org/mod_mbox/maven-users";
 	public static final int BUFFER_SIZE = 1024;
 	
+	public static final String PERSIST_FILE_NAME = "persist.properties";	
+
+	public static String SAVE_ON_LOCAL_STORAGE = "LocalStorage";
+	public static String SAVE_ON_DATABASE = "DB";
+	public static String SEND_OVER_SERVICE = "WebService";
 }

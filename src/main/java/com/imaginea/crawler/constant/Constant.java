b7bece24abe14c2efe407ac1428ff6e8a675457b
maninder-singh <nsit.maninder@gmail.com>
2015-08-11 10:52:21 +0530

    add config and filter field value

--- src/main/java/com/imaginea/crawler/constant/Constant.java
+++ src/main/java/com/imaginea/crawler/constant/Constant.java
@@ -5,9 +5,13 @@ public class Constant {
 	public static final String URL = "http://mail-archives.apache.org/mod_mbox/maven-users";
 	public static final int BUFFER_SIZE = 1024;
 	
-	public static final String PERSIST_FILE_NAME = "persist.properties";	
-
+	public static final String PERSIST_FILE_NAME = "persist.properties";
+	public static final String CONFIG_FILE_NAME = "config.properties";
+	public static final String FILTER_PARAMETER_NAME = "filter-parameter";
+	
 	public static String SAVE_ON_LOCAL_STORAGE = "LocalStorage";
 	public static String SAVE_ON_DATABASE = "DB";
 	public static String SEND_OVER_SERVICE = "WebService";
 }
+
+

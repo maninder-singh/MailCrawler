f4ad575484d7cd0773552bbcd5b71f064e6d9da5
maninder-singh <nsit.maninder@gmail.com>
2015-08-13 13:37:22 +0530

    add fetchRequiredContent method

--- src/main/java/com/imaginea/crawler/util/HtmlParseDataUtil.java
+++ src/main/java/com/imaginea/crawler/util/HtmlParseDataUtil.java
@@ -1,14 +1,20 @@
 package com.imaginea.crawler.util;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.imaginea.crawler.pojo.Data;
 import com.imaginea.crawler.pojo.Mail;
 
 public class HtmlParseDataUtil {
@@ -20,12 +26,12 @@ public class HtmlParseDataUtil {
 	private static final String DATE = ".date";
 	private static final String CONTENTS = ".contents";
 	
-	public static HashSet<String> fetchLinkFromHtmlPage(String url) throws IOException{
+	public static HashSet<String> fetchLinksFromHtmlPage(String url) throws IOException {
 		HashSet<String> urlSet = new HashSet<String>();
 		Document document = Jsoup.connect(url).get();
 		Elements links = document.select(AHREF);
 		for (Element element : links) {
-			urlSet.add(element.attr(HREF));
+			urlSet.add(element.absUrl(HREF));
 		}
 		return urlSet;
 	}
@@ -48,5 +54,29 @@ public class HtmlParseDataUtil {
 		mail.setDate(date.text());
 		
 		return mail;
+	}
+	
+	public static List<Data> fetchRequiredContent(String url,Pattern pattern,List<String> parameterList) throws IOException{
+		List<Data> dataList = new ArrayList<Data>();
+		Data data;
+		Document document = Jsoup.connect(url).get();
+		Element element;
+		boolean isPatternFound;
+		
+		isPatternFound = RegexUtil.isPatternFound(pattern, document.text());
+		if(isPatternFound){
+			for(String parameter : parameterList){
+				data = new Data();
+				element = document.select("." + parameter).first();
+				if(element != null){
+					data.setKey(parameter);
+					data.setValue(element.text());
+				}else{
+					data.setValue("");
+				}
+				dataList.add(data);
+			}
+		}
+		return dataList;
 	}
 }
